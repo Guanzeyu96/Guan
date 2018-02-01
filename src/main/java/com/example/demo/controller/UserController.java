7dc1789134f24ea967ae920fd7bc3bde48e8ccb9
together <zhouucchao@163.com>
2018-02-01 14:32:47 +0800

    5634f

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -59,6 +59,7 @@ public class UserController {
         @RequestMapping("getall")
         @ResponseBody
         public List<User> getAll(){
+        	
         	return userService.getAll();
         }
         
